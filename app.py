57e31abb971895576e91b219bd53c7fc8b224bb1
John Flores <johnflores1991@hotmail.com>
2023-09-27 14:53:07 -0500

    implemented add() and sub() GET query string functionality

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-# Put your app in here.
+from flask import Flask
+app = Flask(__name__)
+
+from flask import request
+import operations
+
+@app.get("/add")
+def add():
+    """return query terms summed"""
+    a = float(request.args['a'])
+    b = float(request.args['b'])
+
+    return str(operations.add(a, b))
+
+@app.get("/sub")
+def sub():
+    """return query terms subtracted"""
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return str(operations.sub(a, b))
